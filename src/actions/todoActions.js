24b6f19cdd542e1a34eb1c9652ee0e92f518cf0a
Nelson-E-S <nelson.e.sanabria1@gmail.com>
2020-08-12 17:37:56 -0400

    finalized redux implementation

--- src/actions/todoActions.js
+++ src/actions/todoActions.js
@@ -1,5 +1,6 @@
 export const UPDATE_TODO_LIST = 'UPADTE_TODO_LIST';
 
-export const upateTodoList = () => ({
-    type: UPDATE_TODO_LIST
+export const updateTodoList = (item) => ({
+    type: UPDATE_TODO_LIST,
+    payload: item
 })
